a852c73bcc68cbbcac5853cbe8fd9f0c77782441
Chloe0001 <chloe88huang@gmail.com>
2021-10-08 14:35:32 +0800

    Add answerHidden variable, changeAnswerHidden, and getAnswerHidden methods. Update toString according to answerHidden.

--- src/main/java/entity/Card.java
+++ src/main/java/entity/Card.java
@@ -8,6 +8,7 @@ public class Card {
     private final String id;
     private String question;
     private String answer;
+    private boolean answerHidden;
     private float proficiency;
 
     /**
@@ -23,6 +24,7 @@ public class Card {
         this.id = id;
         this.question = question;
         this.answer = answer;
+        this.answerHidden = false;
         // TODO: Note proficiency may initialize to a different value
         // proficiency represents to what extent the user has mastered this card
         this.proficiency = 0;
@@ -60,7 +62,11 @@ public class Card {
         this.proficiency -= 20;
     }
 
-    public String getID() {
+    public void changeAnswerHidden() {
+        this.answerHidden = !this.answerHidden;
+    }
+
+    public String getId() {
         return this.id;
     }
 
@@ -72,19 +78,29 @@ public class Card {
         return this.answer;
     }
 
+    public boolean getAnswerHidden() {
+        return this.answerHidden;
+    }
+
     public float getProficiency() {
         return this.proficiency;
     }
 
     @Override
     public String toString() {
-        return String.format("ID: %1$s\nQuestion: %2$s\nAnswer: %3$s",
-                this.id, this.question, this.answer);
+        if (this.answerHidden) {
+            return String.format("Question: %s", this.question);
+        } else {
+            return String.format("Question: %1$s\nAnswer: %2$s",
+                    this.question, this.answer);
+        }
     }
 
-    public static void main(String[] args) {
-        Card c = new Card("1231", "card", "卡");
-        System.out.println(c);
-    }
+//    Simple test
+//    public static void main(String[] args) {
+//        Card c = new Card("1231", "card", "卡");
+//        c.changeHideAnswer();
+//        System.out.println(c);
+//    }
 
 }
