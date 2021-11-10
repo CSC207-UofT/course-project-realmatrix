1692c6131957be9290bf003785bdb4fe605c6287
ziqishu <ziqi.shu@mail.utoronto.ca>
2021-11-10 13:37:02 -0500

    Create LearnGeneratorTest and ReviewGeneratorTest. Todo: in ReviewGenerator class, comments need to specify what kind of cards should be included in the returning list.

---
+++ src/test/java/ReviewGeneratorTest.java
@@ -0,0 +1,87 @@
+import constants.Constants;
+import entity.Card;
+import entity.Pack;
+import manager.CardManager;
+import manager.PackManager;
+import org.junit.Before;
+import org.junit.Test;
+import use_case.ReviewGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class ReviewGeneratorTest {
+    CardManager cm = new CardManager();
+    PackManager pm = new PackManager();
+    Pack p1;
+    Card c1;
+    Card c2;
+    Card c3;
+    Card c4;
+    ReviewGenerator rg;
+
+    /**
+     * Create rg with card1 of proficiency 2, card2 of proficiency 3, card3 of proficiency 0, card4 of proficiency 5.
+     */
+    @Before
+    public void createReviewGenerator() throws Exception {
+        c1 = cm.createNewCard("card1Term", "card1Definition");
+        cm.setCurrCard(c1);
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        c2 = cm.createNewCard("card2Term", "card2Definition");
+        cm.setCurrCard(c2);
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        c3 = cm.createNewCard("card3Term", "card3Definition");
+        c4 = cm.createNewCard("card4Term", "card4Definition");
+        cm.setCurrCard(c4);
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        cm.increaseProficiency();
+        p1 = pm.createNewPack("packName");
+        pm.setCurrPack(p1);
+        pm.addCard(c1);
+        pm.addCard(c2);
+        pm.addCard(c3);
+        pm.addCard(c4);
+        rg = new ReviewGenerator(p1);
+    }
+
+    /**
+     * Test doable based on the created ReviewGenerator, which should return a list of c1, c2, c3 and c4.
+     */
+    @Test
+    public void testDoable(){
+        ArrayList<Card> expectedList = new ArrayList<>();
+        expectedList.add(c1);
+        expectedList.add(c2);
+        expectedList.add(c3);
+        expectedList.add(c4);
+        assertEquals(expectedList, rg.doable());
+    }
+
+    /**
+     * Test withProficiencyBasedCards based on the created ReveiwGenerator.
+     */
+    @Test
+    public void testWithProficiencyBasedCards(){
+        ArrayList<Card> expectedList = new ArrayList<>();
+        for (int i = 0; i < (Constants.REVIEW_PROFICIENCY_MAX - 2); i++) {
+            expectedList.add(c1);
+        }
+        for (int i = 0; i < (Constants.REVIEW_PROFICIENCY_MAX - 3); i++) {
+            expectedList.add(c2);
+        }
+        for (int i = 0; i < (Constants.REVIEW_PROFICIENCY_MAX); i++) {
+            expectedList.add(c3);
+        }
+        assertEquals(expectedList, rg.withProficiencyBasedCards());
+    }
+}
