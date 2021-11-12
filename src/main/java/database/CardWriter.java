55b0ba1ee717be23ea22f1d00d38b983f2ba149d
ChloeH88 <chloe88huang@gmail.com>
2021-11-12 23:37:08 +0800

    1. Add IDataInFactory and IDataOutFactory interfaces for usecase classes to use. 2. Change parameters of all write and archive methods to: <ProgramState> and <Object> (Card/Pack/User), so that make IDataInFactory interface sharable between different writers. 3. Further improvement: consider adding a factory

--- src/main/java/database/CardWriter.java
+++ src/main/java/database/CardWriter.java
@@ -1,5 +1,6 @@
 package database;
 
+import Controller.ProgramState;
 import entity.Card;
 import entity.Pack;
 import entity.User;
@@ -10,37 +11,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
-public class CardWriter {
+public class CardWriter extends Writer {
 
     /**
      * Write or update a card
-     * @param user the user whose package whose card to write/update
-     * @param pack the package whose card to write/update
-     * @param card the card to write/update
+     * @param state the state the program is in
+     * @param o the object (card) to write/update
      * @throws IOException
      */
-    public void writeCard(User user, Pack pack, Card card) throws IOException {
-        new File("user_data/users/" + user.getName() + "/packages/" + pack.getName() + "/cards/").mkdirs();
+    @Override
+    public void write(ProgramState state, Object o) throws IOException {
+        getName(state);     // get the path name we should write into
+        Card c = (Card) o;
+        new File("user_data/users/" + this.username + "/packages/" + this.packname + "/cards/").mkdirs();
         BufferedWriter writer =
-                new BufferedWriter(new FileWriter("user_data/users/" + user.getName()
-                        + "/packages/" + pack.getName() + "/cards/" + card.getTerm() + ".txt"));
-        writer.write(card.getId() + "," + card.getTerm() + ","
-                + card.getDefinition() + "," + card.getProficiency());
+                new BufferedWriter(new FileWriter("user_data/users/" + this.username
+                        + "/packages/" + this.packname + "/cards/" + c.getTerm() + ".txt"));
+        writer.write(c.getId() + "," + c.getTerm() + ","
+                + c.getDefinition() + "," + c.getProficiency());
         writer.close();
     }
 
     /**
      * Archive a card. Effectively, this card is deleted because it won't be loaded next time the program runs.
-     * @param user the user whose package whose card to archive
-     * @param pack the package whose card to archive
-     * @param card the card to archive
+     * @param state the state the program is in
+     * @param o the object (card) to archive
      * @throws IOException
      */
-    public void archiveCard(User user, Pack pack, Card card) throws IOException {
-        new File("user_data/users/" + user.getName() + "/packages/" + pack.getName() +
+    @Override
+    public void archive(ProgramState state, Object o) throws IOException {
+        getName(state);     // get the path name we should write into
+        Card card = (Card) o;
+        new File("user_data/users/" + this.username + "/packages/" + this.packname +
                 "/archived_cards/").mkdirs();
-        Files.move(new File("user_data/users/" + user.getName() + "/packages/" + pack.getName() + "/cards/" +
-                        card.getTerm() + ".txt").toPath(), new File("user_data/users/" + user.getName() +
-                        "/packages/" + pack.getName() + "/archived_cards/" + card.getTerm() + ".txt").toPath());
+        Files.move(new File("user_data/users/" + this.username + "/packages/" + this.packname + "/cards/" +
+                        card.getTerm() + ".txt").toPath(), new File("user_data/users/" +  this.username +
+                        "/packages/" +  this.packname + "/archived_cards/" + card.getTerm() + ".txt").toPath());
     }
 }
--- src/main/java/database/PackWriter.java
+++ src/main/java/database/PackWriter.java
@@ -1,5 +1,6 @@
 package database;
 
+import Controller.ProgramState;
 import entity.Pack;
 import entity.User;
 
@@ -9,18 +10,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
-public class PackWriter {
+public class PackWriter extends Writer {
 
     /**
      * Write or update a package
-     * @param user the user whose package to write/update
-     * @param pack the package to write/update
+     * @param state the state the program is in
+     * @param o the object (pack) to write/update
      * @throws IOException
      */
-    public void writePack(User user, Pack pack) throws IOException {
-        new File("user_data/users/" + user.getName() + "/packages/" + pack.getName()).mkdirs();
+    @Override
+    public void write(ProgramState state, Object o) throws IOException {
+        getName(state);     // get the path name we should write into
+        Pack pack = (Pack) o;
+        new File("user_data/users/" + this.username + "/packages/" + pack.getName()).mkdirs();
         BufferedWriter writer =
-                new BufferedWriter(new FileWriter("user_data/users/" + user.getName()
+                new BufferedWriter(new FileWriter("user_data/users/" + this.username
                         + "/packages/" + pack.getName() + "/package_info.txt"));
         writer.write(pack.getId() + "," + pack.getName());
         writer.close();
@@ -28,14 +32,17 @@ public class PackWriter {
 
     /**
      * Archive a package. Effectively, this package is deleted because it won't be loaded next time the program runs.
-     * @param user the user whose package to archive
-     * @param pack the package to archive
+     * @param state the state the program is in
+     * @param o the object (pack) to archive
      * @throws IOException
      */
-    public void archivePack(User user, Pack pack) throws IOException {
-        new File("user_data/users/" + user.getName() + "/archived_packages/").mkdirs();
-        Files.move(new File("user_data/users/" + user.getName() + "/packages/" + pack.getName()).toPath(),
-                new File("user_data/users/" + user.getName() + "/archived_packages/" +
+    @Override
+    public void archive(ProgramState state, Object o) throws IOException {
+        getName(state);     // get the path name we should write into
+        Pack pack = (Pack) o;
+        new File("user_data/users/" + this.username + "/archived_packages/").mkdirs();
+        Files.move(new File("user_data/users/" + this.username + "/packages/" + pack.getName()).toPath(),
+                new File("user_data/users/" + this.username + "/archived_packages/" +
                         pack.getName()).toPath());
     }
 }
