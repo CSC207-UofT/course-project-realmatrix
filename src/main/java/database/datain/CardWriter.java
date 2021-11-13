5c1a2cb17f3dc47dbf42064086a4f31010422e46
ChloeH88 <90373791+ChloeH88@users.noreply.github.com>
2021-11-13 11:32:29 +0800

    Improve Writer classes inheritance: All subwriter classes inherit write/archive from Writer class.

--- src/main/java/database/datain/CardWriter.java
+++ src/main/java/database/datain/CardWriter.java
@@ -10,40 +10,43 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 public class CardWriter extends Writer {
+    private final Card card;
+
+    /**
+     * Construct a CardWriter object.
+     * @param state the state the program is in
+     * @param o the object (card) to write/update
+     */
+    public CardWriter(ProgramState state, Object o) {
+        super(state, o);
+        this.card = (Card) o;
+    }
 
     /**
      * Write or update a card
-     * @param state the state the program is in
-     * @param o the object (card) to write/update
      * @throws IOException
      */
     @Override
-    public void write(ProgramState state, Object o) throws IOException {
-        getName(state);     // get the path name we should write into
-        Card c = (Card) o;
+    public void write() throws IOException {
         new File("user_data/users/" + this.username + "/packages/" + this.packname + "/cards/").mkdirs();
         BufferedWriter writer =
                 new BufferedWriter(new FileWriter("user_data/users/" + this.username
-                        + "/packages/" + this.packname + "/cards/" + c.getTerm() + ".txt"));
-        writer.write(c.getId() + "," + c.getTerm() + ","
-                + c.getDefinition() + "," + c.getProficiency());
+                        + "/packages/" + this.packname + "/cards/" + this.card.getTerm() + ".txt"));
+        writer.write(this.card.getId() + "," + this.card.getTerm() + ","
+                + this.card.getDefinition() + "," + this.card.getProficiency());
         writer.close();
     }
 
     /**
      * Archive a card. Effectively, this card is deleted because it won't be loaded next time the program runs.
-     * @param state the state the program is in
-     * @param o the object (card) to archive
      * @throws IOException
      */
     @Override
-    public void archive(ProgramState state, Object o) throws IOException {
-        getName(state);     // get the path name we should write into
-        Card card = (Card) o;
+    public void archive() throws IOException {
         new File("user_data/users/" + this.username + "/packages/" + this.packname +
                 "/archived_cards/").mkdirs();
         Files.move(new File("user_data/users/" + this.username + "/packages/" + this.packname + "/cards/" +
-                        card.getTerm() + ".txt").toPath(), new File("user_data/users/" +  this.username +
-                        "/packages/" +  this.packname + "/archived_cards/" + card.getTerm() + ".txt").toPath());
+                        this.card.getTerm() + ".txt").toPath(), new File("user_data/users/" +  this.username +
+                        "/packages/" +  this.packname + "/archived_cards/" + this.card.getTerm() + ".txt").toPath());
     }
 }
