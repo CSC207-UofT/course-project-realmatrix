b017d5a9aaa186e0846a505f8af1d46028e39011
ChloeH88 <90373791+ChloeH88@users.noreply.github.com>
2021-11-13 11:37:09 +0800

    Add design patter factory and factory interface for usecase usage

--- src/main/java/database/datain/IDataInFactory.java
+++ src/main/java/database/datain/IDataInFactory.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 /**
  * An interface for writing and archiving into database.
  */
-public interface IDataWriter {
+public interface IDataInFactory {
     // Write the object into database
-    public void write(ProgramState state, Object o) throws IOException;
+    public void write() throws IOException;
 
     // Archive the object in database (store in database but won't load in future)
-    public void archive(ProgramState state, Object o) throws IOException;
+    public void archive() throws IOException;
 }
