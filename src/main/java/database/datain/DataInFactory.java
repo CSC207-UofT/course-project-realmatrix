9ef77ff8f0d7f4d6ce1ef4d6033ef535c1882a13
ChloeH88 <90373791+ChloeH88@users.noreply.github.com>
2021-11-13 11:37:09 +0800

    Add design patter factory and factory interface for usecase usage

--- src/main/java/database/datain/DataInFactory.java
+++ src/main/java/database/datain/DataInFactory.java
@@ -1,18 +1,52 @@
 package database.datain;
 
 import Controller.ProgramState;
+import entity.*;
 
 import java.io.IOException;
 
-public class DataInFactory implements IDataWriter{
+/**
+ * A class that determines generating which Writer class to write/archive objects.
+ * Usecase classes should only make use of this factory, not specific Writer class.
+ */
+public class DataInFactory implements IDataInFactory{
+    private Writer writer;  // The Writer we should generate
 
-    @Override
-    public void write(ProgramState state, Object o) throws IOException {
-        // TODO: implement
+    /**
+     * Construct a DataInFactory according to the object needs to be written/archived.
+     * @param state the current state the program is in
+     * @param o the object needs to write/archive
+     */
+    // TODO: does checking data type follow clean architecture principles?
+    public DataInFactory(ProgramState state, Object o) {
+        if (o instanceof Card) {
+            this.writer = new CardWriter(state, o);
+        }
+
+        else if (o instanceof Pack) {
+            this.writer = new PackWriter(state, o);
+        }
+
+        else if (o instanceof User) {
+            this.writer = new UserWriter(state, o);
+        }
     }
 
+    /**
+     * Use the writer class to write the object into database.
+     * @throws IOException
+     */
     @Override
-    public void archive(ProgramState state, Object o) throws IOException {
-        // TODO: implement
+    public void write() throws IOException {
+        this.writer.write();
+    }
+
+    /**
+     * Use the writer class to write archive object into database.
+     * @throws IOException
+     */
+    @Override
+    public void archive() throws IOException {
+        this.writer.archive();
     }
 }
