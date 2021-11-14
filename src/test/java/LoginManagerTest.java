a96e3d9d0ba9feeeb6f92d6b2ae01960c2522568
ziqishu <ziqi.shu@mail.utoronto.ca>
2021-11-14 01:46:45 -0500

    Update LoginManagerTest; Todo: test Signoff

--- src/test/java/LoginManagerTest.java
+++ src/test/java/LoginManagerTest.java
@@ -1,5 +1,7 @@
+import entity.User;
 import manager.LoginManager;
 import manager.UserManager;
+import presenters.LoginPresenter;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -17,12 +19,20 @@ public class LoginManagerTest {
         lm = new LoginManager(um);
     }
 
-//    @Test
-//    public void testLoggedInUser() throws Exception {
-//        lm.logInUser(user1Name, user1Password);
-//        assertEquals(lm.getCurrUser().getName(), user1Name);
-//    }
-//
-//    //how to test SignOffUser?
+    @Test
+    public void testLoggedInUser() throws Exception {
+        LoginPresenter lp = new LoginPresenter(user1Name);
+        lm.logInUser(user1Name, user1Password, lp);
+        assertEquals(lm.getCurrUser().getName(), user1Name);
+    }
 
+    @Test
+    public void testGetCurrUser() throws Exception{
+        LoginPresenter lp = new LoginPresenter(user1Name);
+        lm.logInUser(user1Name, user1Password, lp);
+        for (User u: um.getItems().values()){
+            assertEquals(lm.getCurrUser(), u);
+        }
+    }
+    //how to test SignOffUser?
 }
