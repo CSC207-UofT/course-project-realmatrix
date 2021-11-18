11e2e94460f50d689ad82c722ddf6d2bf347bb61
Xing Ling <hylxjackson@gmail.com>
2021-11-18 11:20:26 -0500

    changed text prompt in ChangePasswordFrame

--- src/main/java/framework/GUI/user/ChangePasswordFrame.java
+++ src/main/java/framework/GUI/user/ChangePasswordFrame.java
@@ -2,10 +2,8 @@ package framework.GUI.user;
 
 import entity.User;
 import framework.GUI.BasicFrame;
-import interface_adapter.presenters.ChangePresenter;
 import use_case.input_boundaries.UserInputBoundary;
 import use_case.manager.UserManager;
-import use_case.output_boundaries.ChangeOutputBoundary;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,7 +28,7 @@ public class ChangePasswordFrame extends BasicFrame implements ActionListener {
         // 1. Create components shown on the frame
         changePasswordPanel = new JPanel(new GridLayout(3, 1));
 
-        message = new JLabel("Change username", SwingConstants.CENTER);
+        message = new JLabel("Change password", SwingConstants.CENTER);
         message.setFont(new Font("verdana", Font.BOLD | Font.ITALIC, 38));
 
         newPassword = new JTextField(20);
