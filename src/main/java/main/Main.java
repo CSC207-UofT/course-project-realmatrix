edb9e9250e6df65fb3fe07bd94c50d27a05e9315
ChloeH88 <chloe88huang@gmail.com>
2021-11-22 23:43:03 +0800

    Delete `commandLineInterface`

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,12 +1,14 @@
 package main;
 
-import framework.command_line_interface.CommandLineInterface;
+
+import entity.ProgramState;
+import entity.User;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        CommandLineInterface cli = new CommandLineInterface();
-        cli.display();
+//        CommandLineInterface cli = new CommandLineInterface();
+//        cli.display();
 
 //         Use framework.command_line_interface.GUI
 //        new StartFrame();
