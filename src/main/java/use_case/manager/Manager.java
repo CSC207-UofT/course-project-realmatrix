f69c4374ba3d99cc9eb04f1405adc3fe3b531998
Yifan Zhao <ethany.zhao@mail.utoronto.ca>
2021-11-23 03:13:35 -0500

    updated manager abstract class put add/delete/update/search basic methods

--- src/main/java/use_case/manager/Manager.java
+++ src/main/java/use_case/manager/Manager.java
@@ -1,18 +1,73 @@
 package use_case.manager;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
  * An abstract manager.
  */
 public abstract class Manager<T> {
-    private final HashSet<T> items;
+    private final HashMap<String,T> items;
 
     public Manager() {
-        this.items = new HashSet<>();
+        this.items = new HashMap<>();
     }
 
-    public HashSet<T> getItems() {
+    public HashMap<String,T> getItems() {
         return this.items;
+    }
+
+    /**
+     * to add a new item, we call this method without pass existing name in items.
+     * @param name: unique name
+     * @param item: new item
+     *
+     * @return true iff we successfully add a new item
+     */
+    public boolean addItem(String name, T item){
+        if(!this.items.containsKey(name)){
+            this.items.put(name, item);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * delete a existing item
+     * @param name: item name we want to delete
+     * @return true iff we successfully deleted an item
+     */
+    public boolean deleteItem(String name){
+        if(this.items.containsKey(name)){
+            this.items.remove(name);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * change an existing item in items
+     * @param name item name we want to change
+     * @param newItem new item
+     * @return true iff we successfully changed to new item
+     */
+    public boolean updateItem(String name, T newItem){
+        if(this.items.containsKey(name)){
+            this.items.put(name,newItem);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * search item entity by item name
+     * @param name item name we want
+     * @return the item entity
+     */
+    public T searchItem(String name){
+        return this.items.get(name);
     }
 }
