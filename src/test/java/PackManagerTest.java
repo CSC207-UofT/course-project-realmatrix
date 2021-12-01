8acaf9adfc36bb582e5909586f3497874df9e736
ziqishu <ziqi.shu@mail.utoronto.ca>
2021-12-01 15:37:30 -0500

    Update CardControllerTest, PackManagerTest, UserManagerTest, PackTest, UserTest, ProgramStateTest, LoaderTest.

--- src/test/java/PackManagerTest.java
+++ src/test/java/PackManagerTest.java
@@ -1,17 +1,23 @@
 import entity.Pack;
 import entity.User;
+import interface_adapter.presenters.ChangePresenter;
 import interface_adapter.presenters.SortSearchPackPresenter;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import use_case.input_boundaries.ProgramStateInputBoundary;
 import use_case.manager.ProgramStateManager;
 import use_case.output_boundaries.AddOutputBoundary;
+import use_case.output_boundaries.ChangeOutputBoundary;
 import use_case.output_boundaries.SortSearchPackOutputBoundary;
 import interface_adapter.presenters.AddPresenter;
 import use_case.manager.PackManager;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class PackManagerTest {
     PackManager pm;
@@ -22,6 +28,10 @@ public class PackManagerTest {
     final AddOutputBoundary addOutputBoundary = new AddPresenter();
     final SortSearchPackOutputBoundary sortSearchPackOutputBoundary = new SortSearchPackPresenter();
     final ProgramStateInputBoundary programStateInputBoundary = new ProgramStateManager();
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
     @Before
     public void createPackManager() {
@@ -35,7 +45,7 @@ public class PackManagerTest {
     }
 
     @Test(timeout = 50)
-    public void TestCreateNewPack() {
+    public void testAddNewPack() {
         assertEquals(pName1, p1.getName());
         assertEquals(pName2, p2.getName());
     }
@@ -44,6 +54,44 @@ public class PackManagerTest {
     public void testDeletePack() {
         pm.deletePack(pName2);
         assertNull(programStateInputBoundary.getCurrUser().getPackageMap().get(pName2));
+    }
+
+    @Test
+    public void testChangePackName(){
+        ChangePresenter changeP = new ChangePresenter();
+        String pName2New = "pName2New";
+        assertTrue(pm.changePackName(pName2New, changeP));
+        assertEquals(pName2New, p2.getName());
+        assertFalse(pm.changePackName(pName1, changeP));
+        assertEquals(pName2New, p2.getName());
+    }
+
+
+
+    @Test
+    public void testSearchPack(){
+        pm.searchPack(pName1, sortSearchPackOutputBoundary);
+        ArrayList<String> expect = new ArrayList<>();
+        expect.add(pName1);
+        assertEquals(expect, sortSearchPackOutputBoundary.getSortSearchResult());
+    }
+
+    @Test
+    public void testSortOldToNew(){
+        pm.sortOldToNew(sortSearchPackOutputBoundary);
+        ArrayList<String> expect = new ArrayList<>();
+        expect.add(pName1);
+        expect.add(pName2);
+        assertEquals(expect, sortSearchPackOutputBoundary.getSortSearchResult());
+    }
+
+    @Test
+    public void testSortAToZ(){
+        pm.sortAtoZ(sortSearchPackOutputBoundary);
+        ArrayList<String> expect = new ArrayList<>();
+        expect.add(pName1);
+        expect.add(pName2);
+        assertEquals(expect, sortSearchPackOutputBoundary.getSortSearchResult());
     }
 //
 //    @Test
--- src/test/java/PackTest.java
+++ src/test/java/PackTest.java
@@ -1,54 +1,76 @@
-//import entity.Card;
-//import entity.Pack;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class PackTest {
-//    final String name = "COG250";
-//    Pack p;
-//
-//    @Before
-//    public void createPack() {
-//        p = new Pack(name);
-//    }
-//
-//    @Test
-//    public void TestAddCard() {
-//        Card c = new Card("fragmentation", "don't feel himself as a unit");
-//        Card d = new Card("equivocation", "explaining things by assuming itself");
-//        try {
-//            p.addCard(c);
-//            p.addCard(d);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        List<Card> expected = new ArrayList<>();
-//        expected.add(c);
-//        expected.add(d);
-//        assertEquals(expected, p.getCardList());
-//    }
-//
-//    @Test
-//    public void TestGetName() {
-//        assertEquals(name, p.getName());
-//    }
-//
-//    @Test
-//    public void TestGetCard() {
-//        List<Card> expected = new ArrayList<>();
-//        assertEquals(expected, p.getCardList());
-//    }
-//
-//    @Test
-//    public void TestChangeName() {
-//        String newName = "COG250 Terminology";
-//        p.changeName(newName);
-//        assertEquals(newName, p.getName());
-//    }
-//
-//}
+import entity.Card;
+import entity.Pack;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PackTest {
+    final String name = "COG250";
+    Pack p;
+
+    @Before
+    public void createPack() {
+        p = new Pack(name);
+    }
+
+    @Test
+    public void testAddCard() {
+        Card c = new Card("fragmentation", "don't feel himself as a unit");
+        Card d = new Card("equivocation", "explaining things by assuming itself");
+        try {
+            p.addCard(c);
+            p.addCard(d);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        List<Card> expected = new ArrayList<>();
+        expected.add(c);
+        expected.add(d);
+        assertEquals(expected, p.getCardList());
+    }
+
+    @Test
+    public void testDeleteCard(){
+        Card c = new Card("fragmentation", "don't feel himself as a unit");
+        p.addCard(c);
+        List<Card> expected = new ArrayList<>();
+        expected.add(c);
+        assertEquals(expected, p.getCardList());
+        p.deleteCard(c);
+        assertEquals(0, p.getCardList().size());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals(name, p.getName());
+    }
+
+    @Test
+    public void testGetCardList() {
+        List<Card> expected = new ArrayList<>();
+        assertEquals(expected, p.getCardList());
+    }
+
+    @Test
+    public void testGetCardMap(){
+        Card c = new Card("fragmentation", "don't feel himself as a unit");
+        p.addCard(c);
+        HashMap<String, Card> expect = new HashMap<>();
+        expect.put(c.getTerm(), c);
+        assertEquals(expect, p.getCardMap());
+    }
+
+    @Test
+    public void testChangeName() {
+        String newName = "COG250 Terminology";
+        p.changeName(newName);
+        assertEquals(newName, p.getName());
+    }
+
+}
